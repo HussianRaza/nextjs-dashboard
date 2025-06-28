0cf78f43403246101f9857983f60f5ad7f41491b
Hussain <razahussain876@gmail.com>
2025-06-29 00:37:17 +0500

    added error page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,20 +27,20 @@ const createInvoice = async (formData: FormData) => {
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-
+  } catch (error) {
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 };
 
-// Use Zod to update the expected types
+//zod validation
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
-// ...
 
 const updateInvoice = async (id: string, formData: FormData) => {
   const { customerId, amount, status } = UpdateInvoice.parse({
@@ -50,19 +50,25 @@ const updateInvoice = async (id: string, formData: FormData) => {
   });
 
   const amountInCents = amount * 100;
-
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-
+  } catch (error) {
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 };
 
 const deleteInvoice = async (id: string) => {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
 };
 
